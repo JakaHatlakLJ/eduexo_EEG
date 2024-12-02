f7f998e9fa4447ebfdeb177b188211ca6706d764
JAKA <hatlakjaka@gmail.com>
2024-12-02 17:15:55 +0100

    Corrected motor parameters interpretation

--- LSL_send.py
+++ LSL_send.py
@@ -1,4 +1,3 @@
-import time
 #import socket
 from dynamixel_sdk import *  # Import the Dynamixel SDK
 from gpiozero import LED
@@ -32,27 +31,24 @@ PROTOCOL_VERSION = 2.0  # Dynamixel protocol version (1.0 or 2.0)
 ADDR_PRESENT_POSITION = 132  # Address for reading current position (for Dynamixel Pro series, use 132)
 ADDR_PRESENT_CURRENT = 126
 ADDR_PRESENT_VELOCITY = 128
-ADDR_PRESENT_INPUT_VOLTAGE = 144
-ADDR_PRESENT_PWM = 124
-ADDR_PRESENT_TEMPERATURE = 146
 LENGTH = 4  # Length of the data (4 bytes for position data)
 
 # Setup UDP communication (PC IP address and port)
-UDP_IP = "192.168.1.44"  # Replace with your PC's IP address
-UDP_PORT = 5005  # Port on which the data will be received
+#UDP_IP = "192.168.1.44"  # Replace with your PC's IP address
+#UDP_PORT = 5005  # Port on which the data will be received
 
 # Unit conversion
 pos_unit = 360/4095         # [deg] = [dxl_unit] * [pos_unit]
-vel_unit = 2 * np.pi/60     # [deg/s]
+vel_unit = 2 * np.pi/60     # [rad/s]
 cur_unit = 2.69             # [mA]
-vol_unit = 0.1              # [V]
+
 
 
 # Create a UDP socket
 #sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Create a new stream info
-info = StreamInfo('Stream_EXO', 'EEG', 6, 100, 'float32', 'test_LSL')
+info = StreamInfo('Stream_EXO', 'EEG', 4, 100, 'float32', 'test_LSL')
 # Create an outlet
 outlet = StreamOutlet(info)
 
@@ -78,14 +74,18 @@ def get_motor_position():
     if dxl_comm_result != COMM_SUCCESS:
         print(f"Error: {packet_handler.getTxRxResult(dxl_comm_result)}")
         return None
+    b1 = dxl_present_position.to_bytes(4, byteorder=sys.byteorder, signed = False) 
+    dxl_present_position = int.from_bytes(b1, byteorder=sys.byteorder, signed = True)
     return dxl_present_position
 
 def get_motor_current():
     """Get the current current of the Dynamixel motor"""
-    dxl_present_current, dxl_comm_result, dxl_error = packet_handler.read4ByteTxRx(port_handler, DXL_ID, ADDR_PRESENT_CURRENT)
+    dxl_present_current, dxl_comm_result, dxl_error = packet_handler.read2ByteTxRx(port_handler, DXL_ID, ADDR_PRESENT_CURRENT)
     if dxl_comm_result != COMM_SUCCESS:
         print(f"Error: {packet_handler.getTxRxResult(dxl_comm_result)}")
         return None
+    b1 = dxl_present_current.to_bytes(2, byteorder=sys.byteorder, signed = False) 
+    dxl_present_current = int.from_bytes(b1, byteorder=sys.byteorder, signed = True)    
     return dxl_present_current
 
 def get_motor_velocity():
@@ -94,31 +94,9 @@ def get_motor_velocity():
     if dxl_comm_result != COMM_SUCCESS:
         print(f"Error: {packet_handler.getTxRxResult(dxl_comm_result)}")
         return None
+    b1 = dxl_present_velocity.to_bytes(4, byteorder=sys.byteorder, signed = False) 
+    dxl_present_velocity = int.from_bytes(b1, byteorder=sys.byteorder, signed = True)    
     return dxl_present_velocity
-
-def get_motor_VoltageIN():
-    """Get the current Input Voltage of the Dynamixel motor"""
-    dxl_present_voltageIN, dxl_comm_result, dxl_error = packet_handler.read4ByteTxRx(port_handler, DXL_ID, ADDR_PRESENT_INPUT_VOLTAGE)
-    if dxl_comm_result != COMM_SUCCESS:
-        print(f"Error: {packet_handler.getTxRxResult(dxl_comm_result)}")
-        return None
-    return dxl_present_voltageIN
-
-def get_motor_PWM():
-    """Get the current PWM of the Dynamixel motor"""
-    dxl_present_PWM, dxl_comm_result, dxl_error = packet_handler.read4ByteTxRx(port_handler, DXL_ID, ADDR_PRESENT_PWM)
-    if dxl_comm_result != COMM_SUCCESS:
-        print(f"Error: {packet_handler.getTxRxResult(dxl_comm_result)}")
-        return None
-    return dxl_present_PWM
-
-def get_motor_temp():
-    """Get the current temperature of the Dynamixel motor"""
-    dxl_present_temp, dxl_comm_result, dxl_error = packet_handler.read4ByteTxRx(port_handler, DXL_ID, ADDR_PRESENT_TEMPERATURE)
-    if dxl_comm_result != COMM_SUCCESS:
-        print(f"Error: {packet_handler.getTxRxResult(dxl_comm_result)}")
-        return None
-    return dxl_present_temp
 
 
 led = LED(27)       # Turn LED on
@@ -136,17 +114,13 @@ try:
             position = get_motor_position()
             velocity = get_motor_velocity()
             current = get_motor_current()
-            temp = get_motor_temp()
-            voltageIN = get_motor_VoltageIN()
-            pwm = get_motor_PWM()
+            torque = 0.082598 * (1 - (1 - current * cur_unit / 8.247191)**2) #[mNm]
             
-            DATA = [position * pos_unit, velocity * vel_unit, current * cur_unit, temp, voltageIN * vol_unit, pwm]
-
+            DATA = [position * pos_unit, velocity * vel_unit, current * cur_unit, torque]
             # Send motor position data via stream
             outlet.push_sample(DATA)
             #message = str(position).encode('utf-8')
-            #sock.sendto(message, (UDP_IP, UDP_PORT))
-            time.sleep(1)
+            #sock.sendto(message, (UDP_IP, UDP_PORT)) 
 
 except KeyboardInterrupt:
     print("Program interrupted")
